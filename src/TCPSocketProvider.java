b5969a9c735598a4a35d77b8e1d3c062160c4b5b
mikhail.polivakha <mikhail.polivakha@rt.ru>
2021-05-02 13:13:52 +0300

    create Socket Reader support (for tcp socket only)

--- src/TCPSocketProvider.java
+++ src/TCPSocketProvider.java
@@ -1,22 +1,21 @@
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class TCPSocketProvider {
 
-    private ServerSocket serverSocket;
+    private static ServerSocket serverSocket;
 
     public TCPSocketProvider() {
         try {
-            this.serverSocket = new ServerSocket(8711);
+            serverSocket = new ServerSocket(8711);
         } catch (IOException exception) {
             exception.printStackTrace();
         }
     }
 
-    public void open() throws IOException {
+    public static void open() throws IOException {
         final Socket socket = serverSocket.accept();
         final InputStream inputStream = socket.getInputStream();
         byte[] buffer = new byte[1024];
