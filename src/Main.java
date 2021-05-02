ed876188f4cac840cdabc453b74d3e126fbd501f
mikhail.polivakha <mikhail.polivakha@rt.ru>
2021-05-02 12:32:43 +0300

    append some breaking changes to master

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Alright");
+    public static void main(String[] args) throws IOException {
+        var fileReader = new FileReader(new File("/etc/group"));
+        var bufferedReader = new BufferedReader(fileReader);
+        System.out.println(bufferedReader.readLine());
     }
 }
